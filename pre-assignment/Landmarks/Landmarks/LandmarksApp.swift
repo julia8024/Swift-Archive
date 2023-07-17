80a3e142c47ffbf0492569953c52e6d08983eeb4
julia8024 <julia8024@naver.com>
2023-07-17 21:11:33 +0900

    pre-assignment

---
+++ pre-assignment/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 장세희 on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
