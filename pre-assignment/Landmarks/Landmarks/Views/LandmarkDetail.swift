0b9961c7067977e11944c5e99100c7e9257501d6
julia8024 <julia8024@naver.com>
2023-07-24 21:28:51 +0900

    complete pre-assignment

--- pre-assignment/Landmarks/Landmarks/Views/LandmarkDetail.swift
+++ pre-assignment/Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
-    
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
     
     var body: some View {
         ScrollView {
@@ -24,8 +28,12 @@ struct LandmarkDetail: View {
             
             VStack(alignment: .leading) {
                 
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                   Text(landmark.name)
+                       .font(.title)
+                   FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+               }
+                
                 HStack {
                     Text(landmark.park)
                     Spacer()
@@ -51,7 +59,10 @@ struct LandmarkDetail: View {
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+
     static var previews: some View {
-        LandmarkDetail(landmark: landmarks[0])
+        LandmarkDetail(landmark: modelData.landmarks[0])
+            .environmentObject(modelData)
     }
 }
